f154c646fe9216408228df4cd682d85d7975fb7d
FarhanZubir <faehan19@gmail.com>
2022-06-10 17:01:56 +0800

    full

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 define('db_user', 'root');
 define('db_password', '');
 define('db_host', 'localhost');
-define('db_name", "registration');
+define('db_name', 'registration');
 $conn = mysqli_connect (db_host, db_user, db_password, db_name);
 
 if (mysqli_connect_errno())
