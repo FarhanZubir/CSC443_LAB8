ad6efe475922d74b79076421ecb81b535d122983
FarhanZubir <faehan19@gmail.com>
2022-06-10 17:10:51 +0800

    add

---
+++ add_user.php
@@ -0,0 +1,9 @@
+<?php
+include 'conn.php';
+$sql="insert into user (first_name, last_name, email, password) 
+values ('$_POST [firstname]', '$_POST[Lastname]', '$_POST[email]', '$_POST [password]', '$_POST[reg_date]')";
+if(!mysqli_query($conn, $sql)){
+    die('Error:'. mysqli_error());
+}
+echo "1 record successfully added...";
+?>
