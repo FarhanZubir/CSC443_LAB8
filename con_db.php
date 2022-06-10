4e2b22ca915b6bd9bea97bddbd47b33137ee616b
FarhanZubir <faehan19@gmail.com>
2022-06-10 16:36:28 +0800

    commit

--- con_db.php
+++ con_db.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php
 define('db user', 'root');
 define('db_password','');
@@ -22,5 +13,3 @@ foreach ($row as $key => $value) {
 mysqli_free_result ($result);
 mysqli_close($conn);
 ?>
-</body>
-</html>
